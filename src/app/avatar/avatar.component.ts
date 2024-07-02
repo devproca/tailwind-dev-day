ff34aa6b76adfc53ddd85439a5c88e1e6bc61bf1
Nickkack <nickknack17@gmail.com>
2024-07-02 15:58:19 -0600

    merge

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -5,7 +5,13 @@ import {Component, Input} from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './avatar.component.html',
-  styleUrl: './avatar.component.scss'
+  styleUrl: './avatar.component.scss',
+  styles: [`
+    :host {
+      display: inline-block;
+    }
+  `
+  ]
 })
 export class AvatarComponent {
   @Input() label: string = "";
