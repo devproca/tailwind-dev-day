0e19c8248c4f38b8fe9732955cf47a92b6b597b4
Nickkack <nickknack17@gmail.com>
2024-07-02 15:14:53 -0600

    4b-answer update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {SvgButtonComponent} from "./svg-button/svg-button.component";
 import {SvgComponent} from "./svg/svg.component";
 import {ChipComponent} from "./chip/chip.component";
 import {AlertComponent} from "./alert/alert.component";
+import {HomeScreenComponent} from "./home-screen/home-screen.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AvatarComponent, SvgButtonComponent, SvgComponent, ChipComponent, AlertComponent],
+  imports: [RouterOutlet, AvatarComponent, SvgButtonComponent, SvgComponent, ChipComponent, AlertComponent, HomeScreenComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
